b260d248538e2e24a7cc179358b632f29b98fa82
Zet <111925382+zetmosoma10@users.noreply.github.com>
2024-02-17 20:46:28 +0200

    add Typography

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,5 @@
 function App() {
-  return (
-    <>
-      <h1>App Rendered!</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Id ut odio
-        nesciunt porro, quis facilis est accusamus at exercitationem ad ipsam,
-        eius maiores hic corrupti. Vitae omnis soluta veritatis vero amet
-        dolorem accusamus unde animi impedit similique ipsa officiis qui fugiat,
-        minus debitis iusto ad, quibusdam at nisi eius excepturi.
-      </p>
-    </>
-  );
+  return <></>;
 }
 
 export default App;
