70ff47098d0735818e0b1091de8f4588f955b9f6
Zet <111925382+zetmosoma10@users.noreply.github.com>
2024-02-17 21:58:07 +0200

    build callout action component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
 import ButtonLink from "./components/ButtonLink";
+import CallOutAction from "./components/CallOutAction";
 import Feature from "./components/Feature";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <CallOutAction />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/CallOutAction.jsx
@@ -0,0 +1,12 @@
+import ButtonLink from "./ButtonLink";
+
+function CallOutAction() {
+  return (
+    <div className="callout">
+      <h2 className="callout__heading">Find out more about how we work</h2>
+      <ButtonLink type="hero">How we work</ButtonLink>
+    </div>
+  );
+}
+
+export default CallOutAction;
