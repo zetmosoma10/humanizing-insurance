65d875e2deb2ca134acd65fbe67692b1529f6ede
Zet <111925382+zetmosoma10@users.noreply.github.com>
2024-02-19 12:32:03 +0200

    build Navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,12 @@ import CallOutAction from "./components/CallOutAction";
 import Feature from "./components/Feature";
 import Footer from "./components/Footer";
 import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <>
-      <Feature />
+      <Navbar />
     </>
   );
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,44 @@
+import { useState } from "react";
+import ButtonLink from "./ButtonLink";
+
+function Navbar() {
+  const [toggle, setToggle] = useState(false);
+  console.log(toggle);
+  let toggleClass;
+  const handleToggle = () => {
+    console.log("Clicked!");
+    setToggle((prevToggle) => !prevToggle);
+  };
+
+  toggle ? (toggleClass = "collapsable-nav") : (toggleClass = null);
+  return (
+    <nav>
+      <a href="/" className="nav__logo">
+        <img src="images/logo.svg" alt="logo" />
+      </a>
+      <div className="toggle-container" onClick={handleToggle}>
+        {toggle ? (
+          <img className="icon-hamburger" src="images/icon-hamburger.svg" />
+        ) : (
+          <img className="icon-close" src="images/icon-close.svg" />
+        )}
+      </div>
+      <ul className={`nav__links ${toggleClass}`}>
+        <li>
+          <a href="#">HOW WE WORK</a>
+        </li>
+        <li>
+          <a href="#">BLOG</a>
+        </li>
+        <li>
+          <a href="#">ACCOUNT</a>
+        </li>
+        <li>
+          <ButtonLink type="nav">view plans</ButtonLink>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
